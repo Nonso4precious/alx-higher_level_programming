13e84ccda7a620f57eef3db6bc0bedbf3e422a3e
Nonso4precious <nonso4precious@gmail.com>
2024-01-25 05:11:43 +0100

    My second edited task 4

--- 0x04-python-more_data_structures/7-main.py
+++ 0x04-python-more_data_structures/7-main.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
-update_dictionary = __import__('7-update_dictionary').update_dictionary
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+print_sorted_dictionary = __import__('7-print_sorted_dictionary').print_sorted_
+dictionary
+
+a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
 print_sorted_dictionary(new_dict)
 print("--")
